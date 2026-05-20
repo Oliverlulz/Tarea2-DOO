bc3874811ac676a12b91341895429c5da55ff1eb
Bastian <bacabezas2025@udec.cl>
2026-05-20 19:20:50 -0400

    Agregado métodos en la clase Invitacion

--- src/main/java/Invitacion.java
+++ src/main/java/Invitacion.java
@@ -2,4 +2,35 @@ import java.time.Instant;
 
 public class Invitacion {
     private Instant hora;
+    private Invitable invitado;
+
+    public Invitacion(Invitable invitado, Instant hora) {
+        this.hora = hora;
+        this.invitado = invitado;
+    }
+
+    public void enviar() {
+        this.invitado.Invitar();
+    }
+
+    public Instant getHora() {
+        return hora;
+    }
+
+    public void setHora(Instant hora) {
+        this.hora = hora;
+    }
+
+    public Invitable getInvitado() {
+        return invitado;
+    }
+
+    public void setInvitado(Invitable invitado) {
+        this.invitado = invitado;
+    }
+
+    @Override
+    public String toString() {
+        return "Invitación enviada a:\n" + invitado.toString() + "\nHora: " + hora;
+    }
 }

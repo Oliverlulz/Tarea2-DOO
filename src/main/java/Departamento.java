983dd9ff080657ec88d49fc1713c901310700dd1
Bastian <bacabezas2025@udec.cl>
2026-05-19 23:49:36 -0400

    Agregado métodos en clase Departamento

--- src/main/java/Departamento.java
+++ src/main/java/Departamento.java
@@ -1,6 +1,46 @@
+import java.util.ArrayList;
 
 public class Departamento implements Invitable{
     private String nombre;
+    private ArrayList<Empleado> empleados;
 
-    public int obtenerCantidadEmpleados(){}
+    public Departamento(String Nombre) {
+        this.nombre = Nombre;
+        empleados = new ArrayList();
+    }
+
+    public void agregarEmpleado(Empleado e) {
+        empleados.add(e);
+    }
+
+    public int obtenerCantidadEmpleados() {
+        return empleados.size();
+    }
+
+    public String getNombreDepartamento() {
+        return nombre;
+    }
+
+    public void setNombreDepartamento(String Nombre) {
+        this.nombre = Nombre;
+    }
+
+    public ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(ArrayList<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
+    @Override
+    public void Invitar() {
+        System.out.println("Invitación enviada al " + nombre +
+                            " (" + obtenerCantidadEmpleados() + " empleados)");
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", " + "Cantidad de empleados: " + empleados.size();
+    }
 }

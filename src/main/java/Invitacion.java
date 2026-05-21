4077f3b34eb9c4a931a92f97fa7bbab59c79a785
Bastian <bacabezas2025@udec.cl>
2026-05-21 01:18:47 -0400

    Agregado métodos y JavaDoc en clases Invitacion y Retraso

--- src/main/java/Invitacion.java
+++ src/main/java/Invitacion.java
@@ -1,34 +1,61 @@
 import java.time.Instant;
 
+/**
+ * Representa la invitación enviada a un empleado o a un departamento, contiene la hora
+ * en la cual se envió la invitación y el invitado al cual se le envió
+ */
 public class Invitacion {
     private Instant hora;
     private Invitable invitado;
 
+    /**
+     * Crea el registro de la invitación que contiene a quien recibió la invitación
+     * y la hora a la cual se envió
+     * @param invitado La persona a la cual se envió la invitación a la reunión
+     * @param hora La hora en la cual se envió la invitación
+     */
     public Invitacion(Invitable invitado, Instant hora) {
         this.hora = hora;
         this.invitado = invitado;
     }
 
-    public void enviar() {
-        this.invitado.Invitar();
-    }
-
+    /**
+     * Obtiene el momento en el que se envió la invitación
+     * @return El instante en el cual se envió la invitación
+     */
     public Instant getHora() {
         return hora;
     }
 
+    /**
+     * Modifica la hora en la cual se envió la invitación
+     * @param hora La hora a la cual se quiere cambiar
+     */
     public void setHora(Instant hora) {
         this.hora = hora;
     }
 
+    /**
+     * Otorga la persona a la cual se le envió la invitación
+     * @return El invitado en concreto
+     */
     public Invitable getInvitado() {
         return invitado;
     }
 
+    /**
+     * Modifica a la persona que se tiene registro sobre el envío de la invitación
+     * @param invitado La nueva persona que se quiere cambiar
+     */
     public void setInvitado(Invitable invitado) {
         this.invitado = invitado;
     }
 
+    /**
+     * Imprime en pantalla una descripción detallada sobre la invitación que se envió
+     * incluye la persona a la cual se le envió y el momento exacto cuando se envió
+     * @return La descripción que contiene al receptor y el instante cuando se envió
+     */
     @Override
     public String toString() {
         return "Invitación enviada a:\n" + invitado.toString() + "\nHora: " + hora;
--- src/main/java/Retraso.java
+++ src/main/java/Retraso.java
@@ -1,10 +1,49 @@
 import java.time.Instant;
 
+/**
+ * Representa a un registro sobre un participante que asistió a la reunión, pero llegó tarde,
+ * guarda el momento en el cual el participante llegó
+ * a la reunión, extiende el funcionamiento de {@link Asistencia}
+ */
 public class Retraso extends Asistencia {
     private Instant hora;
 
+    /**
+     * Crea el registro de una persona que asistió a la reunión con la hora a la que llegó
+     * y la persona en concreto
+     * @param participante La persona que asistió a la reunión
+     * @param hora La hora a la cual la persona llegó a la reunión
+     */
     public Retraso(Invitable participante, Instant hora) {
         this.hora = hora;
         super(participante);
+    }
+
+    /**
+     * Obtiene la hora a la cual llegó el participante
+     * @return La hora exacta de la asistencia de una persona a la reunión
+     */
+    public Instant getHora() {
+        return hora;
+    }
+
+    /**
+     * Modifica la hora en la cual llegó un participante a la reunión
+     * @param hora La hora a la cual se desea cambiar
+     */
+    public void setHora(Instant hora) {
+        this.hora = hora;
+    }
+
+    /**
+     * Entrega una descripción detallada sobre la asistencia de un participante
+     * el cual llegó tarde a la reunión, incluye quién, el estado de asistencia
+     * y la hora a la cual llegó
+     * @return La descripción sobre la asistencia de un participante
+     */
+    @Override
+    public String toString() {
+        return super.getParticipante().toString() + ", Presente, con retraso\n"
+                + "Llegó a las: " + this.hora.toString();
     }
 }

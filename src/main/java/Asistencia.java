8176bdc924879f8a6ce8291e6eaa69e4d4b35669
Bastian <bacabezas2025@udec.cl>
2026-05-20 20:53:16 -0400

    Agregado métodos a las clases Asistencia y Retraso

--- src/main/java/Asistencia.java
+++ src/main/java/Asistencia.java
@@ -1,2 +1,20 @@
 public class Asistencia {
+    private Invitable participante;
+
+    public Asistencia(Invitable participante) {
+        this.participante = participante;
+    }
+
+    public Invitable getParticipante() {
+        return participante;
+    }
+
+    public void setParticipante(Invitable participante) {
+        this.participante = participante;
+    }
+
+    @Override
+    public String toString() {
+        return participante.toString() + ", Presente a tiempo";
+    }
 }
--- src/main/java/Retraso.java
+++ src/main/java/Retraso.java
@@ -2,4 +2,9 @@ import java.time.Instant;
 
 public class Retraso extends Asistencia {
     private Instant hora;
+
+    public Retraso(Invitable participante, Instant hora) {
+        this.hora = hora;
+        super(participante);
+    }
 }

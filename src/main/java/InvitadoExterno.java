5aae72cf5a8f2fbe0868f6169097523636a9ca66
Bastian <bacabezas2025@udec.cl>
2026-05-21 01:20:37 -0400

    Creación clase, agregado de métodos y JavaDoc para clase InvitadoExterno

---
+++ src/main/java/InvitadoExterno.java
@@ -0,0 +1,90 @@
+/**
+ * Representa a una persona externa que no es un empleado de un departamento
+ * pero que puede ser invitada a una reunión
+ */
+public class InvitadoExterno implements Invitable {
+    private String nombre;
+    private String apellidos;
+    private String correo;
+
+    /**
+     * Crea a una persona externa a un departamento con un nombre, apellidos y un correo
+     * electrónico de contacto
+     * @param nombre El nombre de esta persona
+     * @param apellidos Los apellidos de esta persona
+     * @param correo El correo electrónico donde se le puede contactar
+     */
+    public InvitadoExterno(String nombre, String apellidos, String correo) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+    }
+
+    /**
+     * Obtiene el nombre de la persona
+     * @return El nombre de esta persona externa
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Modifica el nombre de una persona
+     * @param nombre El nombre al cual se quiere cambiar
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Otorga los apellidos de la persona externa
+     * @return Los apellidos de esta persona
+     */
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     * Cambia los apellidos registrados de esta persona
+     * @param apellidos Los o el apellido al cual se desea cambiar
+     */
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    /**
+     * Entrega el correo de contacto de la persona
+     * @return El correo electrónico de esta persona
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * Modifica el correo registrado asociado a esta persona
+     * @param correo El correo modificado de la persona
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * Invita a una persona externa e imprime en pantalla a quién se invitó y dónde
+     * se envió la invitación a la reunión
+     */
+    @Override
+    public void Invitar() {
+        System.out.println("Invitación enviada a " + nombre
+                            + " " + apellidos + " al correo " + correo);
+    }
+
+    /**
+     * Entrega una descripción de la persona en concreto, incluye su nombre, apellidos
+     * y el correo electrónico asociado a la persona
+     * @return La descripción con el nombre, apellidos y correo electrónico de la persona
+     */
+    @Override
+    public String toString() {
+        return nombre + " " + apellidos + "\n" + "Correo: " + correo;
+    }
+}

d31421a769677fe500ab85d54f228c3cc40655f1
Bastian <bacabezas2025@udec.cl>
2026-05-21 01:16:41 -0400

    Agregado JavaDoc en clases Departamento y Empleado

--- src/main/java/Departamento.java
+++ src/main/java/Departamento.java
@@ -1,44 +1,88 @@
 import java.util.ArrayList;
 
+/**
+ * Representa un departamento específico de alguna materia
+ * Contiene una cantidad de empleados que pueden ser invitados a una reunión
+ */
 public class Departamento implements Invitable{
     private String nombre;
     private ArrayList<Empleado> empleados;
 
+    /**
+     * Crea un nuevo departamento con un nombre y sus empleados
+     * @param Nombre el nombre que tiene este departamento
+     */
     public Departamento(String Nombre) {
         this.nombre = Nombre;
         empleados = new ArrayList();
     }
 
-    public void agregarEmpleado(Empleado e) {
-        empleados.add(e);
+    /**
+     * Agrega un empleado al departamento
+     * @param empleado el empleado que se agrega
+     */
+    public void agregarEmpleado(Empleado empleado) {
+        empleados.add(empleado);
     }
 
+    /**
+     * Determina cuantos empleados hay en el departamento
+     * @return la cantidad total de empleados
+     */
     public int obtenerCantidadEmpleados() {
         return empleados.size();
     }
 
+    /**
+     * Obtiene el nombre del departamento
+     * @return el nombre del departamento
+     */
     public String getNombreDepartamento() {
         return nombre;
     }
 
+    /**
+     * Modifica el nombre del departamento
+     * @param Nombre el nombre al cual se desea cambiar
+     */
     public void setNombreDepartamento(String Nombre) {
         this.nombre = Nombre;
     }
 
+    /**
+     * Obtiene la lista de los empleados actuales en el departamento
+     * @return la lista que contiene a los empleados
+     */
     public ArrayList<Empleado> getEmpleados() {
         return empleados;
     }
 
+    /**
+     * Cambia la lista actual de empleados por una nueva
+     * @param empleados la lista nueva de empleados la cual se desea cambiar
+     */
     public void setEmpleados(ArrayList<Empleado> empleados) {
         this.empleados = empleados;
     }
 
+    /**
+     * Invita a todas los empleados dentro del departamento
+     */
     @Override
     public void Invitar() {
+        for (Empleado empleado : this.empleados) {
+            empleado.Invitar();
+        }
+
         System.out.println("Invitación enviada al " + nombre +
-                            " (" + obtenerCantidadEmpleados() + " empleados)");
+                            " (" + obtenerCantidadEmpleados() + " Personas)");
     }
 
+    /**
+     * Imprime en pantalla una descripción del departamento y la cantidad de empleados que
+     * contiene
+     * @return
+     */
     @Override
     public String toString() {
         return nombre + ", " + "Cantidad de empleados: " + empleados.size();
--- src/main/java/Empleado.java
+++ src/main/java/Empleado.java
@@ -1,3 +1,7 @@
+/**
+ * Representa a un empleado perteneciente a un departamento,
+ * que puede ser invitado a una reunión
+ */
 public class Empleado implements Invitable{
     private String id;
     private String apellidos;
@@ -5,6 +9,15 @@ public class Empleado implements Invitable{
     private String correo;
     private Departamento departamento;
 
+    /**
+     * Crea a un empleado con id, nombre, apellidos, correo y
+     * un departamento al cual pertenece
+     * @param id la ID del empleado
+     * @param apellidos los apellidos del empleado
+     * @param nombre el nombre del empleado
+     * @param correo el correo de contacto
+     * @param departamento el departamento al cual pertenece el empleado
+     */
     public Empleado(String id, String apellidos, String nombre
                     , String correo, Departamento departamento) {
         this.id = id;
@@ -14,55 +27,103 @@ public class Empleado implements Invitable{
         this.departamento = departamento;
     }
 
+    /**
+     * Obtiene la ID del empleado
+     * @return La ID asociada al empleado
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Modifica la ID asociada al empleado
+     * @param id La nueva ID que se le quiere asignar al empleado
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Obtiene los apellidos del empleado
+     * @return Los apellidos del empleado
+     */
     public String getApellidos() {
         return apellidos;
     }
 
+    /**
+     * Modifica los apellidos de un empleado
+     * @param apellidos Los apellidos a los que se quiere cambiar
+     */
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
+    /**
+     * Obtiene el correo de contacto del empleado
+     * @return El correo asociado
+     */
     public String getCorreo() {
         return correo;
     }
 
+    /**
+     * Modifica el correo asociado al empleado
+     * @param correo El nuevo correo del empleado
+     */
     public void setCorreo(String correo) {
         this.correo = correo;
     }
 
+    /**
+     * Obtiene el nombre del empleado
+     * @return El nombre de este
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Modifica el nombre de un empleado
+     * @param nombre El nombre al que se quiere cambiar
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Obtiene el departamento al cual el empleado pertenece
+     * @return El departamento en concreto
+     */
     public Departamento getDepartamento() {
         return departamento;
     }
 
+    /**
+     * Modifica el departamento del cual el empleado pertenece
+     * @param departamento El nuevo departamento donde se encuentra el empleado
+     */
     public void setDepartamento(Departamento departamento) {
         this.departamento = departamento;
     }
 
+    /**
+     * Invita a un empleado específico, imprime en pantalla a quien se invitó
+     * y a que correo
+     */
     @Override
     public void Invitar() {
-        System.out.println("Invitación enviada a: " + nombre + " " + apellidos
-                            + ", al correo " + correo);
+        System.out.println("Invitación enviada a " + nombre + " " + apellidos
+                            + " al correo " + correo);
     }
 
+    /**
+     * Entrega una descripción breve de los datos del empleado la cual imprime en pantalla
+     * @return La descripción de los datos del empleado
+     */
     @Override
     public String toString() {
-        return "Nombre empleado: " + nombre + " " + apellidos + "\nID: " +
+        return nombre + " " + apellidos + "\nID: " +
                 id + "\nCorreo: " + correo + "\n" + departamento.getNombreDepartamento();
     }
 }

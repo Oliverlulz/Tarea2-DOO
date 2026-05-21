a13347ed2b1e740bf22aaa9adc949459bc293cdd
Bastian <bacabezas2025@udec.cl>
2026-05-21 01:15:03 -0400

    Agregado métodos y JavaDoc en la clase Asistencia

--- src/main/java/Asistencia.java
+++ src/main/java/Asistencia.java
@@ -1,18 +1,37 @@
+/**
+ * Representa un registro de la asistencia de una persona que asistió a cierta reunión
+ */
 public class Asistencia {
     private Invitable participante;
 
+    /**
+     * Registra que un participante asistió a la reunión
+     * @param participante El participante que asistió
+     */
     public Asistencia(Invitable participante) {
         this.participante = participante;
     }
 
+    /**
+     * Obtiene al participante que asistió a la reunión
+     * @return El participante en concreto
+     */
     public Invitable getParticipante() {
         return participante;
     }
 
+    /**
+     * Modifica al participante asociado al registro
+     * @param participante El participante modificado
+     */
     public void setParticipante(Invitable participante) {
         this.participante = participante;
     }
 
+    /**
+     * Entrega una descripción del participante y su estado en la reunión
+     * @return El participante y su estado de asistencia
+     */
     @Override
     public String toString() {
         return participante.toString() + ", Presente a tiempo";
